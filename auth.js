e09704f76495defd9b17a033cb06ab51bbca1237
MarieVanGreves <keiiskyx@gmail.com>
2023-05-10 17:07:58 +0200

    Added some missing files,

---
+++ auth.js
@@ -0,0 +1,85 @@
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+  timeCost: 5,
+  parallelism: 1,
+};
+
+
+//Hashing a given password (middleware)
+const hashPassword = (req, res, next) => {
+  argon2
+    .hash(req.body.password, hashingOptions)
+    .then((hashedPassword) => {
+      console.log(hashedPassword);
+
+      req.body.hashedPassword = hashedPassword;
+      delete req.body.password;
+
+      next();
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
+//verifying the password matches the hashed version in the database for a given email
+const verifyPassword = (req, res) => {
+    argon2
+      .verify(req.user.hashedPassword, req.body.password)
+      .then((isVerified) => {
+        if (isVerified) {
+          const payload = { sub: req.user.id };
+  
+          const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+          });
+  
+          delete req.user.hashedPassword;
+          res.send({token, user: req.user });
+        } else {
+          res.status(401).send("Wrong Password!");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+  
+
+//Token Verification (generates a token (populate in the OAuth in Auth))
+const verifyToken = (req, res, next) => {
+    try {
+      const authorizationHeader = req.get("Authorization");
+  
+      if (authorizationHeader == null) {
+        throw new Error("Authorization header is missing");
+      }
+  
+      const [type, token] = authorizationHeader.split(" ");
+  
+      if (type !== "Bearer") {
+        throw new Error("Authorization header has not the 'Bearer' type");
+      }
+  
+      req.payload = jwt.verify(token, process.env.JWT_SECRET);
+  
+      next();
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(401);
+    }
+  };
+
+
+module.exports = {
+  hashPassword,
+  verifyPassword,
+  verifyToken
+};
